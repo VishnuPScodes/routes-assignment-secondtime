6a01905fff0b5cb02f7842938bccd4386666d078
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-10 15:13:40 +0530

    gg

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Contact() {
-  return (
-    <div>Contact</div>
-  )
+  return <> 
+    <h2>Feel free to contact us for any queries!</h2>
+    <div style={{marginTop:"50px",display:"flex"}} >
+      <div style={{border:"2px solid teal",height:"200px",width:"200px"}}>
+
+      </div>
+      <div style={{border:"2px solid teal",height:"200px",width:"200px",marginLeft:"80px"}}>
+
+      </div>
+    </div>
+    </>
 }
 
 export default Contact
