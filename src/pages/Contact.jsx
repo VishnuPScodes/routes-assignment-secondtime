1ee192273088cd9a7697f78326fde0542fd4c8f9
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-12 19:49:04 +0530

    ee

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,10 +4,14 @@ function Contact() {
   return <> 
     <h2>Feel free to contact us for any queries!</h2>
     <div style={{marginTop:"50px",display:"flex",margin:"auto"}} id='contact-boxes' >
-      <div style={{border:"2px solid teal",height:"100px",width:"200px",marginLeft:"30%",color:"blue",fontWeight:"700",textAlign:"center",fontSize:"25px",paddingTop:"55px"}}>
+      <div style={{border:"2px solid teal",height:"100px",width:"200px",marginLeft:"30%",color:"blue",fontWeight:"700",textAlign:"center",fontSize:"25px",paddingTop:"55px"}} onClick={(()=>{
+        alert('Sorry we dont have this feature at the moment ,Please try later!')
+      })}>
          Call
       </div>
-      <div style={{border:"2px solid teal",height:"100px",width:"200px",marginLeft:"80px",color:"blue",fontWeight:"700",textAlign:"center",fontSize:"25px",paddingTop:"55px"}}>
+      <div style={{border:"2px solid teal",height:"100px",width:"200px",marginLeft:"80px",color:"blue",fontWeight:"700",textAlign:"center",fontSize:"25px",paddingTop:"55px"}} onClick={(()=>{
+        alert('Sorry we dont have this feature yet ,please try again later!')
+      })}>
         Message
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,48 @@
 
 
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function Home() {
-  return (
+  useEffect(()=>{
+      setTimeout(()=>{
+        let wehave=document.getElementById('wehave');
+        wehave.style.display='block'
+      },1000)
+
+      
+
+      setTimeout(()=>{
+        let fruites=document.getElementById('fruites');
+        fruites.style.display='block'
+      },2000)
+
+      setTimeout(()=>{
+        let wehave2=document.getElementById('wehave2')
+        wehave2.style.display='block'
+      },3000)
+      setTimeout(()=>{
+        let veg=document.getElementById('veg');
+        veg.style.display='block'
+      },4000)
+  },[])
+  return <> 
     <h1>(: Welcome to the Premium website :)</h1>
-  )
+    <div id='wehave'>
+      Here we have 
+    </div>
+    <div  id='fruites'>
+      Fruites
+    </div>
+
+    <div id='wehave2'>
+      AND
+
+    </div>
+    <div id='veg'>
+      Vegitables!
+    </div>
+    </>
 }
 
 export default Home
