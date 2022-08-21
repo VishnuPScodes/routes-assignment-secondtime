c7da4b1e6408e753fbb3462dd394a55aa9fe5a76
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-21 22:32:40 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from 'react'
 
 function Home() {
  const [num,setNum]=useState(0) 
+ console.log(num,'num')
   useEffect(()=>{
       setTimeout(()=>{
         let wehave=document.getElementById('wehave');
@@ -26,6 +27,10 @@ function Home() {
         let veg=document.getElementById('veg');
         veg.style.display='block'
       },4000)
+      setTimeout(()=>{
+        let movie=document.getElementById('movieBar');
+        movie.style.display='block'
+      },5000)
   },[])
   let images=[
     'https://images.squarespace-cdn.com/content/v1/59d7e2c7e45a7c0ce235bb55/1552056695609-2KJMOHURUOTUGJCJUQWQ/Captain-Marvel-Film-Review.jpg',
@@ -36,19 +41,47 @@ function Home() {
    
    
   ]
+  setTimeout(()=>{
+      
+if(num==null){
+
+}
+else if(num==0){
+   let main=document.getElementById('maind');
+   main.style.backgroundColor='#ffedc5'
+}
+else if(num==1){
+  let main=document.getElementById('maind');
+  main.style.backgroundColor='#911115' 
+}
+else if(num==2){
+  let main=document.getElementById('maind');
+  main.style.backgroundColor='#536937'
+}
+else if(num==3){
+  let main=document.getElementById('maind');
+  main.style.backgroundColor='#0121a8'
+}
+else if(num==4){
+  let main=document.getElementById('maind');
+  main.style.backgroundColor='#f21a77'
+}
+
+  },5000)
+
   //var image='https://static.toiimg.com/photo/msid-92954243/92954243.jpg'
-  useEffect(()=>{
-    setInterval(()=>{
-      if(num==images.length){
-        setNum(0);
-      }
-      setNum(num+1)
-    },2000)
+  // useEffect(()=>{
+  //   setInterval(()=>{
+  //     if(num==images.length){
+  //       setNum(0);
+  //     }
+  //     setNum(num+1)
+  //   },2000)
  
-  },[])
+  // },[])
   
  
-  return <> 
+  return <div id='maind'> 
     <h1>(: Welcome to the Premium website :)</h1>
     <div style={{color:"orange"}}  id='wehave'>
       Here we have 
@@ -64,10 +97,39 @@ function Home() {
     <div style={{color:'green'}} id='veg'>
       Vegitables!
     </div>
-    <div id='movieBar'>
+    <div  onClick={(()=>{
+      console.log(num)
+      if(num==images.length){
+        setNum(0)
+      }
+      else{
+       
+        if(num!=images.length-1){
+          setNum(num=>num+1);
+        }
+        else{
+          setNum(0)
+        }
+      }
+      console.log(num)
+    })} id='movieBar'>
         <img src={images[num]} height={'100%'} width='100%' alt="" />
     </div>
-    </>
+    <div>
+      <h1>This is the best place for you! </h1>
+       <h2 onMouseEnter={(()=>{
+          let movie=document.getElementById('movieLover');
+          movie.style.display='block'
+       })}>If you are a</h2>
+       <h2 id='movieLover' onMouseEnter={(()=>{
+        let smiley=document.getElementById('smiley');
+        smiley.style.display='block';
+       })} >Movie Lover!</h2>
+       <h2 id='smiley'>😉</h2>
+    </div>
+    <div ></div>
+    </div>
 }
+
 
 export default Home
