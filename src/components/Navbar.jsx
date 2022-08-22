df71913bbed8edf2f10f9683b772fcb93262f82c
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-22 23:11:26 +0530

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,28 +6,28 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div 
+    <div id='nav-head'
      style={{border:"5px solid blue",height:"80px",width:"99%",color:"red",fontWeight:"700",display:"flex"}}>
         
 
         <div style={{marginLeft:"50px"}}>
-            <Link  to={'/'} >HOME</Link>
+            <Link id='link'  to={'/'}  style={{textDecoration:"none"}}>HOME</Link>
         </div>
 
         <div style={{marginLeft:"50px"}}>
-            <Link  to={'/Seafood'} >Seafood</Link>
+            <Link id='link'  style={{textDecoration:"none"}} to={'/Seafood'} >Seafood</Link>
         </div>
         <div style={{marginLeft:"50px"}}>
-            <Link  to={'/Fruites'} >FRUITES</Link>
+            <Link  id='link' style={{textDecoration:"none"}} to={'/Fruites'} >FRUITES</Link>
         </div>
         <div style={{marginLeft:"50px"}}>
-            <Link to={'/Vegs'} > VEGS </Link>
+            <Link id='link' style={{textDecoration:"none"}} to={'/Vegs'} > VEGS </Link>
         </div>
         <div style={{marginLeft:"50px"}}>
-            <Link to={'/Contacts'} >CONTACT</Link>
+            <Link id='link' style={{textDecoration:"none"}} to={'/Contacts'} >CONTACT</Link>
         </div>
         <div style={{marginLeft:"50px"}}>
-            <Link to={'/FAQ'} >FAQ</Link>
+            <Link id='link' style={{textDecoration:"none"}} to={'/FAQ'} >FAQ</Link>
         </div>
      </div>
   )
--- src/pages/Seafood.jsx
+++ src/pages/Seafood.jsx
@@ -3,26 +3,29 @@ import axios from 'axios'
 import SingleFood from '../components/SingleFood'
 import {useNavigate} from 'react-router-dom'
 function Seafood() {
+  const [loading,setLoading]=useState(true);
   const [data,setData]=useState([]);
   const navigate=useNavigate();
     useEffect(()=>{
       axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood').then((e)=>{
           setData(e.data);
-
+         
+      }).then(()=>{
+         setLoading(false)
       }).catch((err)=>{
         console.log(err)
       })
     },[])
     console.log(data.meals)
-  return (
-    <div style={{display:"flex",flexWrap:"wrap",flexDirection:"row"}}>
-      {data?.meals?.map((e)=>{
-        return <div onClick={(()=>{
-              navigate(`/seaFoodDetails/${e.idMeal}`)
-        })}> <SingleFood  name={e.strMeal} image={e.strMealThumb} /> </div>
-      })}
+  return <div>{loading==true? <h1>Loading...</h1>: <div style={{display:"flex",flexWrap:"wrap",flexDirection:"row"}}>
+  {data?.meals?.map((e)=>{
+    return <div onClick={(()=>{
+          navigate(`/seaFoodDetails/${e.idMeal}`)
+    })}> <SingleFood  name={e.strMeal} image={e.strMealThumb} /> </div>
+  })}
+</div>}  
+   
     </div>
-  )
 }
 
 export default Seafood
