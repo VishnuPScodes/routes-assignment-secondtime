f6b3c5329d61449c97075ec7d0d0b36fa8747373
VishnuPScodes <95961812+VishnuPScodes@users.noreply.github.com>
2022-08-23 21:52:47 +0530

    adde

--- src/pages/SeafoodDetails.jsx
+++ src/pages/SeafoodDetails.jsx
@@ -17,10 +17,11 @@ function SeafoodDetails({image,name}) {
   return (
     <div>
         <div id='s-d'>
-          {data.length!=0 && <img src={data?.meals[0]?.strMealThumb} height={'100%'} width='100%' alt='' /> }
+          {data.length!=0 && <img id='image-d' src={data?.meals[0]?.strMealThumb} height={'100%'} width='100%' alt='' /> }
         </div>
         <h3> {data.length!=0 && <h3>{data.meals[0].strMeal}</h3>}</h3>
-        <p>Instructions : {data.length!=0 && <div style={{width:"60%",margin:'auto'}}>{data.meals[0].strInstructions}</div>}</p>
+        <h4>Instructions:</h4>
+        <div id='text-para'> {data.length!=0 && <div style={{width:"60%",margin:'auto'}}>{data.meals[0].strInstructions}</div>}</div>
     </div>
   )
 }
